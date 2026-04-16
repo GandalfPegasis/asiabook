3abc98f62ef99f2ab38a23c1f51bf6c2e8fd4df5
GandalfPegasis <julianhandoyo5@gmail.com>
2026-04-16 17:26:51 +0800

    simple holder router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 
 router.all("/profile/*", require("./profile"));
 router.all("/forums/*", require("./forum"));
+router.all("/clubs/*", require("./club"));
+router.all("/friends/*", require("./friend"));
 
 module.exports = router;
